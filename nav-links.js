4a1fc58da5ee1503ff501a072dab54b543babbd0
TheTrueWhiteOwl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2024-05-16 07:30:03 +0200

    fix: javascript let added

--- nav-links.js
+++ nav-links.js
@@ -2,7 +2,7 @@ const url = window.location.pathname;
 const filename = url.substring(url.lastIndexOf('/')+1);
 
 function highlightCurrentLink() {
-    currentLinks = document.querySelectorAll('a[href="'+filename+'"]');
+    let currentLinks = document.querySelectorAll('a[href="'+filename+'"]');
     currentLinks.forEach(link => link.className += ' current-link');
 }
 
